7076aa724d4672f7fa5f59394045163e28cc1a85
Kathir <62984098+Kathir797@users.noreply.github.com>
2021-05-06 18:29:05 +0530

    Prototype

---
+++ BW Sync/Assets/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed=10f, jumpPower=10f;
+    public SpriteRenderer sprite;
+
+    Rigidbody2D body;
+    bool isGrounded;
+    float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space)))
+        {
+            body.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //if (isGrounded)
+        //{
+            body.AddForce(transform.right * horizontal * moveSpeed);
+        //}
+        //sprite.flipX = horizontal > 0 ? false : (horizontal < 0 ? true : sprite.flipX);
+    }
+
+    void OnTriggerStay2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Planet 1"))
+        {
+            body.drag = 1f;
+
+            float distance = Mathf.Abs(obj.GetComponent<GravityPoint>().planetRadius - Vector2.Distance(transform.position, obj.transform.position));
+            if (distance < 1f)
+            {
+                isGrounded = distance < 0.93f;
+               // Debug.Log(distance);
+
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Planet 1"))
+        {
+            body.drag = 0.2f;
+        }
+    }
+}
