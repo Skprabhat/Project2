8032b4e232f3c7b1716c9b8204c6c86f7610eb3e
Kathir <62984098+Kathir797@users.noreply.github.com>
2021-06-05 14:08:46 +0530

    Demo

---
+++ BW Sync/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] Positions;
+    public GameObject[] Object;
+
+    private int rand;
+    private int randposition;
+
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+
+            rand = Random.Range(0, Object.Length);
+            randposition = Random.Range(0, Positions.Length);
+            Instantiate(Object[rand], Positions[randposition].transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+    }
+
